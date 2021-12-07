6673fcd973b12e26e237d70120d54c06a415ef64
xbehemoth123 <marin.duni@gmail.com>
2021-12-07 19:38:03 +0100

    k

---
+++ frontend/src/store.js
@@ -0,0 +1,12 @@
+import data from "./data";
+import {applyMiddleware, compose, createStore} from 'redux';
+ // import thunk from redux-thunk;
+
+/*const initialState ={};
+const reducer = (state, action) => {
+    return {products: data.products}
+}
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(reducer, initialState, compose(applyMiddleware(thunk)));
+
+export default store; */
